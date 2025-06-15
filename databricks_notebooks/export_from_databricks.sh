5276d76134a265caec290ccb99ed24e49345b604
thanhthuy250599 <17133064@student.hcmute.edu.vn>
2025-06-16 03:43:09 +0700

    Update export_from_databricks.sh

--- databricks_notebooks/export_from_databricks.sh
+++ databricks_notebooks/export_from_databricks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf databricks_notebooks/clean_simple_covid
+
 # Export notebook từ Databricks Workspace về local
 databricks workspace export \
   "/Users/uongthanhthuy99@gmail.com/clean_simple_covid" \
