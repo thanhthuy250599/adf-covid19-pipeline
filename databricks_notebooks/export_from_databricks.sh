8ac579baccd6cdb01653dca15b05c11076924213
thanhthuy250599 <17133064@student.hcmute.edu.vn>
2025-06-16 03:36:23 +0700

    Update export_from_databricks.sh

--- databricks_notebooks/export_from_databricks.sh
+++ databricks_notebooks/export_from_databricks.sh
@@ -3,6 +3,6 @@
 # Export notebook từ Databricks Workspace về local
 databricks workspace export \
   "/Users/uongthanhthuy99@gmail.com/clean_simple_covid" \
-  "clean_simple_covid.py" \
+  "databricks_notebooks/clean_simple_covid.py" \
   --profile databricks-default \
   --format SOURCE
